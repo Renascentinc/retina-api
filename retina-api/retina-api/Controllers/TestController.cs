f8a560d14e52879f2445b396d8df5ed534e80511
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2016-12-28 22:48:35 -0500

    finishing up end-to-end

--- retina-api/retina-api/Controllers/TestController.cs
+++ retina-api/retina-api/Controllers/TestController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.Http;
 
 namespace retina_api.Controllers
@@ -10,6 +13,51 @@ namespace retina_api.Controllers
         {
 
             return Ok(new List<int>() { 1, 2, 3 });
+        }
+        public dynamic getJSONTool(string id)
+        {
+            dynamic JSONTool = new { id = id };
+            return JSONTool;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(dynamic tool)
+        {
+            try
+            {
+                using
+                (
+                    SqlConnection myConnection = new SqlConnection(
+                        "user id = renascentinc;" +
+                        "password = Skylark777!;" +
+                        "server=retinadbserver.database.windows.net;" +
+                        "database = retina_develop_db;" +
+                        "connection timeout = 30")
+                )
+                {
+                    myConnection.Open();
+                    SqlCommand cmd = new SqlCommand("testproc", myConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ToolID", 222222);
+
+                    SqlDataReader myReader = cmd.ExecuteReader();
+
+                    List<dynamic> myList = new List<dynamic>();
+                    while (myReader.Read())
+                    {
+                        myList.Add(getJSONTool((string)myReader["type"]));
+                    }
+
+                    myConnection.Close();
+                    return Ok(tool);
+                    //return Ok( new { data = myList } );
+                }
+            }
+            catch (Exception e)
+            {
+                return Ok(e);
+            }
+
         }
     }
 }
---
+++ retina-api/retina-api/Controllers/ToolsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace retina_api.Controllers
+{
+    public class ToolsController : ApiController
+    {
+ 
+        //This function takes a db record and converts it into Ember.js combatable JSON
+        private dynamic getJSONTool(IDataRecord record)
+        {
+            dynamic JSONTool = new
+            {
+
+                type = "tool",
+                id = record["ToolID"],
+                attribute = new
+                {
+                        toolClass = ((string) record["Class"]).TrimEnd(' '),
+                             type = ((string) record["Type"]).TrimEnd(' '),
+                            brand = ((string) record["Brand"]).TrimEnd(' '),
+                    datePurchased =           record["DatePurchased"],
+                    purchasedFrom = ((string) record["PurchasedFrom"]).TrimEnd(' '),
+                            price =           record["Price"],
+                      modelNumber = ((string) record["ModelNumber"]).TrimEnd(' '),
+                           status = ((string) record["Status"]).TrimEnd(' '),
+                            photo = ((string) record["Photo"]).TrimEnd(' ')
+                }
+            };
+
+            return JSONTool;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetbyID(int id)
+        {
+            try
+            {
+                using
+                (
+                    SqlConnection myConnection = new SqlConnection(
+                        "user id = renascentinc;" +
+                        "password = Skylark777!;" +
+                        "server=retinadbserver.database.windows.net;" +
+                        "database = retina_develop_db;" +
+                        "connection timeout = 30")
+                )
+                {
+                    myConnection.Open();
+                    SqlCommand cmd = new SqlCommand("testproc", myConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ToolID", id);
+
+                    SqlDataReader myReader = cmd.ExecuteReader();
+
+                    dynamic tool = new { };
+                    while (myReader.Read())
+                    {
+                        tool = getJSONTool((IDataRecord)myReader);
+                    }
+
+                    myConnection.Close();
+                    
+                    return Ok( new { data = tool } );
+                }
+            }
+            catch (Exception e)
+            {
+                return Ok(e);
+            }
+
+        }
+    }
+}
