09ad51d430ac4db64bc37222677c9ffde743099c
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2016-12-31 16:35:35 -0500

    added search contoller

--- retina-api/retina-api/Controllers/ToolsController.cs
+++ retina-api/retina-api/Controllers/ToolsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using retina_api.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -13,64 +14,32 @@ namespace retina_api.Controllers
     public class ToolsController : ApiController
     {
         
-        //This function takes a db record and converts it into Ember.js combatable JSON
-        private dynamic getJSONTool(IDataRecord record)
-        {
-            dynamic JSONTool = new
-            {
-
-                type = "tool",
-                id = record["ToolID"],
-                attribute = new
-                {
-                        toolClass = ((string) record["Class"]).TrimEnd(' '),
-                             type = ((string) record["Type"]).TrimEnd(' '),
-                            brand = ((string) record["Brand"]).TrimEnd(' '),
-                    datePurchased =           record["DatePurchased"],
-                    purchasedFrom = ((string) record["PurchasedFrom"]).TrimEnd(' '),
-                            price =           record["Price"],
-                      modelNumber = ((string) record["ModelNumber"]).TrimEnd(' '),
-                           status = ((string) record["Status"]).TrimEnd(' '),
-                            photo = ((string) record["Photo"]).TrimEnd(' ')
-                }
-            };
-
-            return JSONTool;
-        }
-
+      
         [HttpGet]
         public IHttpActionResult GetbyID(int id)
         {
             try
             {
-                using
-                (
-                    SqlConnection myConnection = new SqlConnection(
-                        "user id = renascentinc;" +
-                        "password = Skylark777!;" +
-                        "server=retinadbserver.database.windows.net;" +
-                        "database = retina_develop_db;" +
-                        "connection timeout = 30")
-                )
+                SqlConnection myConnection = new DBConnector().newConnection;
+                myConnection.Open();
+
+                SqlCommand cmd = new SqlCommand("testproc", myConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ToolID", id);
+
+                SqlDataReader myReader = cmd.ExecuteReader();
+
+                Tool tool = null;
+                while (myReader.Read())
                 {
-                    myConnection.Open();
-                    SqlCommand cmd = new SqlCommand("testproc", myConnection);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.Parameters.AddWithValue("@ToolID", id);
-
-                    SqlDataReader myReader = cmd.ExecuteReader();
-
-                    dynamic tool = new { };
-                    while (myReader.Read())
-                    {
-                        tool = getJSONTool((IDataRecord)myReader);
-                    }
-
-                    myConnection.Close();
-                    
-                    return Ok( new { data = tool } );
+                    tool = new Tool((IDataRecord)myReader);
                 }
+
+                myConnection.Close();
+                    
+                return Ok( new { data = tool } );
+
             }
             catch (Exception e)
             {
