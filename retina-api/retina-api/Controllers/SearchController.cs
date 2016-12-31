eef59f75bd9720a6648d94446f183fa3e8af3910
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2016-12-31 16:36:01 -0500

    added unstaged file

---
+++ retina-api/retina-api/Controllers/SearchController.cs
@@ -0,0 +1,45 @@
+﻿using retina_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace retina_api.Controllers
+{
+    public class SearchController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult search(string status, int userID, string type)
+        {
+
+            SqlConnection myConnection = new DBConnector().newConnection;
+            myConnection.Open();
+
+            SqlCommand cmd = new SqlCommand("??????", myConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Status", status);
+            cmd.Parameters.AddWithValue("@UserID", userID);
+            cmd.Parameters.AddWithValue("@Type", type);
+
+            SqlDataReader myReader = cmd.ExecuteReader();
+
+            List<Tool> toolList = new List<Tool>();
+
+            while (myReader.Read())
+            {
+                toolList.Add(new Tool((IDataRecord)myReader));
+            }
+
+            myConnection.Close();
+
+            return Ok(new { data = toolList });
+
+    
+        }
+    }
+}
