c1bc33364b40f0f30f67dd928e2a4811003aa7ab
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2017-01-02 22:35:58 -0500

    adding providersand users

---
+++ retina-api/retina-api/Controllers/ProvidersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace retina_api.Controllers
+{
+    public class ProvidersController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult getOwners()
+        {
+
+            try
+            {
+
+                List<string> uniqueStrings = new Models.UniqueNames("select_purchased_from").getUniqueStrings("PurchasedFrom");
+                return Ok(new { providers = uniqueStrings });
+
+            }
+            catch (Exception e)
+            {
+                return Ok(e);
+            }
+        }
+    }
+}
---
+++ retina-api/retina-api/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using retina_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace retina_api.Controllers
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult getOwners()
+        {
+
+            try
+            {
+
+                List<dynamic> uniqueDynamics = new UniqueNames("select_user").getUniqueDynamics();
+                return Ok(new { users = uniqueDynamics });
+
+            }catch (Exception e)
+            {
+                return Ok(e);
+            }
+        }
+       
+    }
+}
