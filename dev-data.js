ed1c0af0e4ebb789852e77cbe818ed5b61f31481
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-08-09 08:04:15 -0400

    adding db client

---
+++ dev-data.js
@@ -0,0 +1,27 @@
+
+let devData = {
+  tool: {
+    [
+      {
+        id: 1,
+        type: 'Drill',
+        brand: 'Bosch',
+        purchase_date: '09/09/1998'
+      },
+      {
+        id: 2,
+        type: 'Hammer',
+        brand: 'Hitachi',
+        purchase_date: '02/06/1990'
+      },
+      {
+        id: 3,
+        type: 'Sawzall',
+        brand: 'Dewalt',
+        purchase_date: '04/04/1998'
+      }
+    ]
+  }
+}
+
+module.exports = { devData };
