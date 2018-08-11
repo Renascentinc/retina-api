f12413f6013c456c994cf7463ca306e702acf7c7
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-08-11 12:05:27 -0400

    cleaning

--- data/dev-data.js
+++ data/dev-data.js
@@ -2,19 +2,19 @@
 let devData = {
   tool: [
     {
-      id: 1,
+      // id: 1,
       type: 'Drill',
       brand: 'Bosch',
       purchase_date: '09/09/1998'
     },
     {
-      id: 2,
+      // id: 2,
       type: 'Hammer',
       brand: 'Hitachi',
       purchase_date: '02/06/1990'
     },
     {
-      id: 3,
+      // id: 3,
       type: 'Sawzall',
       brand: 'Dewalt',
       purchase_date: '04/04/1998'
@@ -23,17 +23,17 @@ let devData = {
 
   user: [
     {
-      id: 1,
+      // id: 1,
       first_name: 'Bob',
       last_name: 'Thomas'
     },
     {
-      id: 2,
+      // id: 2,
       first_name: 'Fred',
       last_name: 'Jones'
     },
     {
-      id: 3,
+      // id: 3,
       first_name: 'Riley',
       last_name: 'Roebuck'
     }
