67e5aefb5b772c33ce80720bcd6dab5a43d13847
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-08-11 11:51:47 -0400

    seeding database

---
+++ data/dev-data.js
@@ -0,0 +1,43 @@
+
+let devData = {
+  tool: [
+    {
+      id: 1,
+      type: 'Drill',
+      brand: 'Bosch',
+      purchase_date: '09/09/1998'
+    },
+    {
+      id: 2,
+      type: 'Hammer',
+      brand: 'Hitachi',
+      purchase_date: '02/06/1990'
+    },
+    {
+      id: 3,
+      type: 'Sawzall',
+      brand: 'Dewalt',
+      purchase_date: '04/04/1998'
+    }
+  ],
+
+  user: [
+    {
+      id: 1,
+      first_name: 'Bob',
+      last_name: 'Thomas'
+    },
+    {
+      id: 2,
+      first_name: 'Fred',
+      last_name: 'Jones'
+    },
+    {
+      id: 3,
+      first_name: 'Riley',
+      last_name: 'Roebuck'
+    }
+  ]
+}
+
+module.exports = { devData };
