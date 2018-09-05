4a01d9eb4a23c1e814da83a97be57ccf76fb551a
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-09-05 00:57:14 -0400

    experimenting with login flow

--- server.js
+++ server.js
@@ -5,10 +5,31 @@ const appConfig = require('./app-config');
 const logger = require('./logger');
 const { GraphQlError } = require('./error');
 const express = require('express');
+const bodyParser = require('body-parser');
+const gql = require('graphql-tag');
+
+function tokenValid(token) {
+  return token == 13;
+}
+
+function isLoginRoute(req) {
+  try {
+    const parsedRequest = gql(req.body.query);
+    return parsedRequest.definitions.length == 1 &&
+           parsedRequest.definitions[0].selectionSet.selections.length == 1 &&
+           parsedRequest.definitions[0].selectionSet.selections[0].name.value == 'login'
+  } catch (_) {
+    return false
+  }
+}
 
 function authenticate(req, res, next) {
-  console.log('Authenticating...');
-  next();
+  if (!(tokenValid(req.headers.authorization) || isLoginRoute(req))) {
+    console.log("NOT VALID!!");
+    next(); // DON'T do next() here in real life
+  } else {
+    next();
+  }
 }
 
 class Server {
@@ -29,6 +50,7 @@ class Server {
     try {
       let app = express();
 
+      app.use(bodyParser.json());
       app.use(authenticate);
 
       let apolloServer = new ApolloServer({
