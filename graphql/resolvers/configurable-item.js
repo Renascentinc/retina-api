75a061f1641865b49c764ed4a3f0e27d1d8a1047
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-09-16 21:59:27 -0400

    adding some config item endpoints

--- graphql/resolvers/configurable-item.js
+++ graphql/resolvers/configurable-item.js
@@ -2,9 +2,23 @@
 
 module.exports = {
   Query: {
-     getAllConfigurableItem: async (_, x, db) => {
-       let result = await db.get_all_configurable_item({organization_id: 1});
+     getAllConfigurableItem: async (_, __, { db, session }) => {
+       let result = await db.get_all_configurable_item({
+         organization_id: session.organization_id
+       });
        return result;
+     },
+
+     getConfigurableItem: async (_, { configurable_item_id }, { db, session }) => {
+       let result = await db.get_configurable_item({
+         configurable_item_id,
+         organization_id: session.organization_id
+       });
+       return result[0];
      }
+  },
+
+  Mutation: {
+
   }
 };
