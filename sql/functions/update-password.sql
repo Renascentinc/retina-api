045fac4398aa399cf9aeda26f39729cf1fdb6c68
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-09-17 20:32:00 -0400

    adding update password

---
+++ sql/functions/update-password.sql
@@ -0,0 +1,21 @@
+
+CREATE OR REPLACE FUNCTION public.update_password (
+	user_id          id_t,
+	current_password long_str_t,
+  new_password     long_str_t,
+  organization_id  id_t
+)
+RETURNS SETOF public.user
+AS $$
+  BEGIN
+    RETURN QUERY
+    UPDATE public.user
+    	SET
+    		password = crypt(update_password.new_password, gen_salt('bf'))
+    	WHERE public.user.id = update_password.user_id
+        AND public.user.organization_id = update_password.organization_id
+        AND public.user.password = crypt(update_password.current_password, public.user.password)
+    RETURNING *;
+  END;
+$$
+LANGUAGE plpgsql;
