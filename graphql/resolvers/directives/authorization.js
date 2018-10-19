d1b37d59c19c54ae3b7c0ddb1f452c5a3d16007a
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-10-18 22:23:45 -0400

    fixing async

--- graphql/resolvers/directives/authorization.js
+++ graphql/resolvers/directives/authorization.js
@@ -5,9 +5,10 @@ const { userHasRole } = require('graphql/utils/authorization-utils');
 module.exports = {
   requiresRole: {
 
-    visitFieldDefinition: async (_, { context: { db, session } }, { requiredRole }) =>  {
-      if (!userHasRole(session, db.role.fromString(requiredRole), db)) {
-        throw new AuthorizationError(`User doesn't have required role ${requiredRole}`);
+    visitFieldDefinition: async (fieldInfo, { context: { db, session } }, { requiredRole }) =>  {
+      let userAuthorized = await userHasRole(session, db.role.fromString(requiredRole), db);
+      if (!userAuthorized) {
+        throw new AuthorizationError(`User doesn't have required role ${requiredRole} for field ${fieldInfo.fieldDetails.objectType}::${fieldInfo.field.name}`);
       }
     }
 
