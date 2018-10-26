d7c870b1c43d76b3e6a98176106508da6592971a
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-10-25 20:27:34 -0400

    adding allowed tool id transfers

--- sql/functions/transfer-tool.sql
+++ sql/functions/transfer-tool.sql
@@ -1,15 +1,28 @@
+
+
+/**
+ * For each passed in tool id, update only those tools that are in the organization
+ * and those tools that are owned by the transferrer, if the transferrer is not
+ * an admin and the tool owner type is not a location; else, all tools can be transferred
+ * Each tool that is updated is added to the updated_tool_ids list. Finally,
+ * return all the tools that were updated.
+ */
 CREATE OR REPLACE FUNCTION public.transfer_tool (
-	tool_id_list    integer[] NOT NULL,
-	to_owner_id 	  id_t      NOT NULL,
-  organization_id id_t      NOT NULL,
-  transferrer_id  id_t      NOT NULL
+	tool_id_list    integer[],
+	to_owner_id 	  id_t,
+  organization_id id_t,
+  transferrer_id  id_t
 )
 RETURNS SETOF public.tool
 AS $$
   DECLARE
-    tool_id id_t;
+    tool_id          id_t;
+    updated_tool_id  integer;
+    updated_tool_ids integer[];
   BEGIN
     FOREACH tool_id IN ARRAY tool_id_list LOOP
+      updated_tool_id = NULL;
+
       UPDATE public.tool
         SET
           owner_id = to_owner_id,
@@ -29,15 +42,20 @@ AS $$
           AND public.tool.organization_id = transfer_tool.organization_id
           AND public.tool.owner_id =
             CASE
-              WHEN (SELECT role FROM public.user WHERE id = transferrer_id) = 'ADMINISTRATOR'::role OR
-                   (SELECT owner_type FROM public.tool WHERE id = tool_id) = 'LOCATION'::tool_owner_type
-              THEN public.tool.owner_id
-              ELSE transferrer_id
-            END;
+              WHEN (SELECT role FROM public.user WHERE id = transferrer_id) != 'ADMINISTRATOR'::role AND
+                   (SELECT owner_type FROM public.tool WHERE id = tool_id) != 'LOCATION'::tool_owner_type
+              THEN transferrer_id
+              ELSE public.tool.owner_id
+            END
+        RETURNING id INTO updated_tool_id;
+
+        IF updated_tool_id IS NOT NULL THEN
+          updated_tool_ids = array_append(updated_tool_ids, updated_tool_id);
+        END IF;
     END LOOP;
 
     RETURN QUERY SELECT * FROM tool
-      WHERE tool.id in (SELECT * from unnest(tool_id_list))
+      WHERE tool.id in (SELECT * from unnest(updated_tool_ids))
         AND tool.organization_id = transfer_tool.organization_id;
   END;
 $$
