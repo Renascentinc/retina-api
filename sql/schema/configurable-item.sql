62aa5332e0424372bf209e55b862f4c6c9b3381b
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-11-11 18:00:14 -0500

    adding deleted config item functionality

--- sql/schema/configurable-item.sql
+++ sql/schema/configurable-item.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS public.configurable_item (
 	"type" 					configurable_item_type	NOT NULL,
 	"name" 					citext    							NOT NULL,
 	sanctioned 			boolean 								NOT NULL,
+  deleted         boolean                 NOT NULL DEFAULT false,
 	organization_id id_t 										NOT NULL,
   PRIMARY KEY (id, organization_id),
   CONSTRAINT unique_type_name UNIQUE (type, name, organization_id)
