39ef15969dedeef3311f6c3a13cd46df4e2a80fd
Elias Kraihanzel <Elias Kraihanzel@DESKTOP-I41Q3K0>
2018-12-02 00:27:42 -0500

    making location required

--- sql/schema/location.sql
+++ sql/schema/location.sql
@@ -6,10 +6,11 @@ CREATE TABLE IF NOT EXISTS public."location" (
 	zip 							zip_t 								NOT NULL,
 	address_line_one	long_str_t 						NOT NULL,
   organization_id   id_t                  NOT NULL,
+  "name" 						str_t                 NOT NULL,
 	address_line_two 	long_str_t,
-	"name" 						str_t,
+
   PRIMARY KEY (id, organization_id),
-  CONSTRAINT session_unique_city_state_zip_address_line_one UNIQUE (city, state, zip, address_line_one, organization_id)
+  CONSTRAINT session_unique_city_state_zip_address_line_one UNIQUE (city, state, zip, address_line_one, "name", organization_id)
 ) INHERITS (tool_owner);
 
 CREATE UNIQUE INDEX IF NOT EXISTS location_id_index ON public.location (id);
